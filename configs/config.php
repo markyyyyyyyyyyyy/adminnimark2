34062957fec3e5f0cad53c033a5a7a73ab1fcf7a
markyyyyyyyyyyyy <161038977+markyyyyyyyyyyyy@users.noreply.github.com>
2024-04-25 03:41:09 +0800

    Add files via upload

---
+++ configs/config.php
@@ -0,0 +1,14 @@
+<?php 
+
+$hostname = 'localhost';
+$uname = 'root';
+$password = '';
+$database = 'users_db';
+
+$conn = new mysqli($hostname, $uname, $password, $database);
+
+if(!$conn){
+echo 'mali code';
+} 
+    
+
